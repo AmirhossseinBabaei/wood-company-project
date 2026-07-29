2ed960ee998cb40ab236fe5d51daebda03159e14
= <=>
2026-07-29 09:54:49 +0330

    Add login service for module Authentication

--- Modules/Authentication/app/Http/Services/LoginService.php
+++ Modules/Authentication/app/Http/Services/LoginService.php
@@ -2,7 +2,27 @@
 
 namespace Modules\Authentication\Http\Services;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
 class LoginService
 {
+    public function login(array $data): array
+    {
+        $user = User::query()
+            ->where('email', $data['email'])
+            ->first();
 
+        if (Hash::check($data['password'], $user->password)) {
+            $token = $user->createToken('api');
+
+            $token->accessToken->forceFill([
+                'expires_at' => now()->addDay(),
+            ])->save();
+
+            return ['user' => $user, 'token' => $token->plainTextToken];
+        }
+
+        return [];
+    }
 }
