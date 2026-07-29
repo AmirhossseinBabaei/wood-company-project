eb237216c739036a29027606bd1705ec5161875c
= <=>
2026-07-29 09:56:27 +0330

    Add login routes for module Authentication

--- Modules/Authentication/routes/api.php
+++ Modules/Authentication/routes/api.php
@@ -2,8 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use Modules\Authentication\Http\Controllers\RegisterController;
+use Modules\Authentication\Http\Controllers\LoginController;
 
 Route::prefix('auth')->name('auth.')->group(function () {
     Route::post('register', [RegisterController::class, 'register'])
         ->name('register');
+
+    Route::post('login', [LoginController::class, 'login'])
+        ->name('login');
 });
