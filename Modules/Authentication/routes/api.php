63bbd09b2f3eeb46172cbc8ba098a9544b372f14
Amirhossein Babaei <ab8622822@gmail.com>
2026-07-29 11:19:00 +0330

    Add api route for logout

--- Modules/Authentication/routes/api.php
+++ Modules/Authentication/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Modules\Authentication\Http\Controllers\RegisterController;
 use Modules\Authentication\Http\Controllers\LoginController;
+use Modules\Authentication\Http\Controllers\LogoutController;
 
 Route::prefix('auth')->name('auth.')->group(function () {
     Route::post('register', [RegisterController::class, 'register'])
@@ -10,4 +11,8 @@ Route::prefix('auth')->name('auth.')->group(function () {
 
     Route::post('login', [LoginController::class, 'login'])
         ->name('login');
+
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('logout', [LogoutController::class, 'logout']);
+    });
 });
