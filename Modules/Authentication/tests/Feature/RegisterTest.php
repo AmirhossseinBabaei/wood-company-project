91c24e12dfeaf498c9f2b1df541e111da7b6bd93
Amirhossein Babaei <ab8622822@gmail.com>
2026-07-29 10:45:10 +0330

    Fixing bug register test

--- Modules/Authentication/tests/Feature/RegisterTest.php
+++ Modules/Authentication/tests/Feature/RegisterTest.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Modules\Authentication\Tests\Unit;
+namespace Modules\Authentication\Tests\Feature;
 
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -11,26 +11,28 @@ use Tests\TestCase;
 class RegisterTest extends TestCase
 {
     use RefreshDatabase;
+
     /**
      * A basic test example.
      */
     public function test_user_can_register(): void
     {
-        $user = User::factory()->create()->toArray();
+        $user = User::factory()->make()->toArray();
 
-        $response = $this->post('/auth/register', $user);
+        $response = $this->post('/api/auth/register', $user);
 
         $this->assertDatabaseHas('users', [
-            'email' => 'ali@test.com',
+            'email' => $user['email']
         ]);
-        $response->assertStatus(200);
+
+        $response->assertOk();
     }
 
     public function test_user_can_not_pass_validation_register(): void
     {
         $user = ['email' => 'ali', 'password' => 'laravel12345'];
 
-        $response = $this->post('/auth/register', $user);
-        $response->assertStatus(422);
+        $response = $this->post('/api/auth/register', $user);
+        $response->assertStatus(302);
     }
 }
