adad0041281b9ee5c609d93976718b3be03571a9
= <=>
2026-07-29 09:51:47 +0330

    Creating login controller for module authentication

--- Modules/Authentication/app/Http/Controllers/LoginController.php
+++ Modules/Authentication/app/Http/Controllers/LoginController.php
@@ -2,58 +2,31 @@
 
 namespace Modules\Authentication\Http\Controllers;
 
+use App\Enums\HttpStatus;
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use Modules\Authentication\Http\Requests\LoginRequest;
+use Modules\Authentication\Http\Services\LoginService;
+use Modules\Authentication\Transformers\LoginResource;
 
 class LoginController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * login user
      */
-    public function index()
+    public function login(LoginRequest $request, LoginService $service): JsonResponse
     {
-        //
+        $status = $service->login($request->validated());
 
-        return response()->json([]);
-    }
+        if (null == $status['token']) {
+            return response()->json([
+                'message' => __('messages.server_error'),
+            ], HttpStatus::INTERNAL_SERVER_ERROR->value);
+        }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-
-        return response()->json([]);
-    }
-
-    /**
-     * Show the specified resource.
-     */
-    public function show($id)
-    {
-        //
-
-        return response()->json([]);
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, $id)
-    {
-        //
-
-        return response()->json([]);
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy($id)
-    {
-        //
-
-        return response()->json([]);
+        return response()->json([
+            'message' => __('messages.login_successfully'),
+            'data' => LoginResource::make($status)
+        ], HttpStatus::CREATED->value);
     }
 }
