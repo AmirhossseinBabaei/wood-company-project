8fc6649d042faa514e22f76efd77d46a1833a3c0
Amirhossein Babaei <ab8622822@gmail.com>
2026-07-29 10:19:38 +0330

    Delete views and add LoginTest for module Authentication

--- Modules/Authentication/tests/Feature/LoginTest.php
+++ Modules/Authentication/tests/Feature/LoginTest.php
@@ -2,17 +2,35 @@
 
 namespace Modules\Authentication\Tests\Feature;
 
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Hash;
 use Tests\TestCase;
 
 class LoginTest extends TestCase
 {
+    use RefreshDatabase;
     /**
      * A basic test example.
      */
-    public function test_the_application_returns_a_successful_response(): void
+    public function test_user_can_login_successfully(): void
     {
-        $response = $this->get('/');
+        $user = User::factory()->create([
+            'password' => Hash::make('secret123')
+        ]);
 
-        $response->assertStatus(200);
+        $postedData = [
+            'email' => $user->email,
+            'password' => 'secret123'
+        ];
+
+        $response = $this->post('/api/auth/login', $postedData);
+
+        $this->assertDatabaseHas('personal_access_tokens', [
+            'tokenable_id' => $user->id,
+            'tokenable_type' => User::class
+        ]);
+
+        $response->assertOk();
     }
 }
