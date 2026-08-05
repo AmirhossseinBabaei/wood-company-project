234c84f555c1878edad364a7b4cdd97c1d37a1a7
AmirHosseinBabaei <ab8622822@gmail.com>
2026-08-05 14:16:28 +0330

    Adding words.php files for User module

---
+++ Modules/User/resources/lang/en/words.php
@@ -0,0 +1,46 @@
+<?php
+
+return [
+
+    'users' => 'Users',
+
+    'user' => 'User',
+
+    'create_user' => 'Create User',
+
+    'edit_user' => 'Edit User',
+
+    'user_details' => 'User Details',
+
+    'name' => 'Name',
+
+    'email' => 'Email',
+
+    'password' => 'Password',
+
+    'actions' => 'Actions',
+
+    'save' => 'Save',
+
+    'update' => 'Update',
+
+    'delete' => 'Delete',
+
+    'view' => 'View',
+
+    'back' => 'Back',
+
+    'cancel' => 'Cancel',
+
+    'created_at' => 'Created At',
+
+    'success_create' => 'User created successfully.',
+
+    'success_update' => 'User updated successfully.',
+
+    'success_delete' => 'User deleted successfully.',
+
+    'no_data' => 'No users found.',
+    'first_name' => 'First name',
+    'last_name' => 'Last name',
+];
