5e523e50d4a8bd30dcb669284de0ceb922ba6bfd
Amirhossein Babaei <ab8622822@gmail.com>
2026-08-05 08:14:47 +0330

    Cleaning project ('deleting unsed modules') for pushing into repo

--- Modules/Hotel/resources/views/index.blade.php
+++
@@ -1,5 +0,0 @@
-<x-hotel::layouts.master>
-    <h1>Hello World</h1>
-
-    <p>Module: {!! config('hotel.name') !!}</p>
-</x-hotel::layouts.master>
--- Modules/Hotel/routes/api.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Modules\Hotel\Http\Controllers\HotelController;
-
-Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
-    Route::apiResource('hotels', HotelController::class)->names('hotel');
-});
