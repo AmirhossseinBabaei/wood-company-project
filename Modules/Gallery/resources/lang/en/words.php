d125e460362b2aeed6127a998cbcde97df06c6c5
AmirHosseinBabaei <ab8622822@gmail.com>
2026-08-05 14:16:44 +0330

    Adding words.php files for Gallery module

---
+++ Modules/Gallery/resources/lang/en/words.php
@@ -0,0 +1,79 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Gallery
+    |--------------------------------------------------------------------------
+    */
+
+    'gallery_created' => 'Gallery created successfully.',
+    'gallery_updated' => 'Gallery updated successfully.',
+    'gallery_deleted' => 'Gallery deleted successfully.',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Fields
+    |--------------------------------------------------------------------------
+    */
+
+    'fa_title' => 'Persian Title',
+    'en_title' => 'English Title',
+
+    'fa_description' => 'Persian Description',
+    'en_description' => 'English Description',
+
+    'sort_order' => 'Sort Order',
+
+    'image' => 'Image',
+
+    'status' => 'Status',
+
+    'active' => 'Active',
+    'inactive' => 'Inactive',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Buttons
+    |--------------------------------------------------------------------------
+    */
+
+    'create' => 'Create',
+    'edit' => 'Edit',
+    'delete' => 'Delete',
+    'show' => 'Show',
+    'save' => 'Save',
+    'update' => 'Update',
+    'back' => 'Back',
+    'cancel' => 'Cancel',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Titles
+    |--------------------------------------------------------------------------
+    */
+
+    'gallery' => 'Gallery',
+    'gallery_list' => 'Gallery List',
+    'create_gallery' => 'Create Gallery',
+    'edit_gallery' => 'Edit Gallery',
+    'gallery_details' => 'Gallery Details',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Confirm
+    |--------------------------------------------------------------------------
+    */
+
+    'delete_confirm' => 'Are you sure you want to delete this item?',
+
+    /*
+     |--------------------------------------------------------------------------
+     | Other
+     |--------------------------------------------------------------------------
+*/
+
+    'actions' => 'Actions',
+    'no_data' => 'No data'
+];
