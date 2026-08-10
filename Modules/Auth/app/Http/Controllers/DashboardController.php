db533d3504b1b96fe92e750d8c96e325cdd93268
AmirHosseinBabaei <ab8622822@gmail.com>
2026-08-10 10:55:15 +0330

    Add new data for dashboard controller

--- Modules/Auth/app/Http/Controllers/DashboardController.php
+++ Modules/Auth/app/Http/Controllers/DashboardController.php
@@ -5,14 +5,32 @@ declare(strict_types=1);
 namespace Modules\Auth\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\View\View;
+use Modules\ContactUs\app\Models\ContactMessage;
+use Modules\Project\Models\Project;
+use Modules\Services\Models\Service;
+use Modules\Slider\Models\Slider;
 
 class DashboardController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * @return View
      */
-    public function dashboard()
+    public function dashboard(): View
     {
-       return view('auth::dashboard');
+        $data = [
+            'counters' => [
+                'users' => User::count(),
+                'projects' => Project::count(),
+                'contact_messages_count' => ContactMessage::count(),
+                'services' => Service::count()
+            ],
+            'latest_projects' => Project::latest()->paginate(5),
+            'latest_services' => Service::latest()->paginate(5),
+            'latest_sliders' => Slider::latest()->paginate(5)
+        ];
+
+       return view('auth::dashboard', compact('data'));
     }
 }
