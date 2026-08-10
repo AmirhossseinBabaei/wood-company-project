de329aeac13299b1465ad11fc98c1ed9089cf988
AmirHosseinBabaei <ab8622822@gmail.com>
2026-08-10 11:00:57 +0330

    Add projects migration

---
+++ Modules/Project/database/migrations/2026_08_08_040905_create_projects_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('fa_name');
+            $table->text('fa_slug');
+
+            $table->string('en_name');
+            $table->text('en_slug');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('projects');
+    }
+};
