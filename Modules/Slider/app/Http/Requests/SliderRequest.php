d2d3558c690091f834aa9d3600974c67d551f87d
AmirHosseinBabaei <ab8622822@gmail.com>
2026-08-10 10:50:11 +0330

    Update SliderRequest.php

---
+++ Modules/Slider/app/Http/Requests/SliderRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Slider\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class SliderRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     */
+    public function rules(): array
+    {
+        return [
+            'fa_title' => ['required', 'max:100'],
+            'en_title' => ['required', 'max:100'],
+            'fa_slug' => ['required', 'max:300'],
+            'en_slug' => ['required', 'max:300'],
+            'image' => ['required', 'mimes:png,svg,jpg,jpeg,gif', 'max:5012']
+        ];
+    }
+
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return auth()->check();
+    }
+}
