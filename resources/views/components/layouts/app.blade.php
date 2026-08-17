7087f503f97c24508034bbdd74ceaffa4031c04c
AmirHosseinBabaei <ab8622822@gmail.com>
2026-08-17 09:30:15 +0330

    Updating layouts/app view to new route requests

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -21,7 +21,7 @@
     </div>
 
     @livewireScripts
-    <script src="{{ asset('js/script.js') }}"></script>
+    <script src="{{ asset('js/app.js') }}"></script>
     <script src="{{ asset('js/main.js') }}"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"></script>
 
