d8c05d337a91eefa0da5636074e660681c495721
AmirHosseinBabaei <ab8622822@gmail.com>
2026-08-17 11:04:04 +0330

    Add DashboardViewService for get data from service and return to the dashboard for show in the dashboard

--- Modules/Auth/app/Http/Services/DashboardViewService.php
+++ Modules/Auth/app/Http/Services/DashboardViewService.php
@@ -1,8 +1,33 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Modules\Auth\app\Http\Services;
+
+use App\Models\User;
+use Modules\ContactUs\app\Models\ContactMessage;
+use Modules\Project\Models\Project;
+use Modules\Services\Models\Service;
+use Modules\Slider\Models\Slider;
 
 class DashboardViewService
 {
+    /**
+     * @return array
+     */
+    public function getDashboardData(): array
+    {
+        return [
+            'counters' => [
+                'users' => User::count(),
+                'projects' => Project::count(),
+                'contact_messages_count' => ContactMessage::count(),
+                'services' => Service::count()
+            ],
 
+            'latest_projects' => Project::latest()->take(3)->get(),
+            'latest_services' => Service::latest()->take(3)->get(),
+            'latest_sliders' => Slider::latest()->take(3)->get()
+        ];
+    }
 }
