1a74dd8cf373fdde5e16909c00d441601fe0e2fb
AmirHosseinBabaei <ab8622822@gmail.com>
2026-08-17 09:33:43 +0330

    Updateing UpdateSettingsRequest

--- Modules/Settings/app/Http/Requests/UpdateSettingsRequest.php
+++ Modules/Settings/app/Http/Requests/UpdateSettingsRequest.php
@@ -16,8 +16,8 @@ class UpdateSettingsRequest extends FormRequest
         return [
             'fa_website_name' => ['nullable', 'max:100'],
             'en_website_name' => ['nullable', 'max:100'],
-            'fa_website_description' => ['nullable', 'max:100'],
-            'en_website_description' => ['nullable', 'max:100'],
+            'fa_website_description' => ['nullable'],
+            'en_website_description' => ['nullable'],
             'logo_src' => ['nullable'],
             'favicon' => ['nullable', 'max:1024'],
             'footer_logo' => ['nullable', 'max:1024'],
@@ -29,7 +29,14 @@ class UpdateSettingsRequest extends FormRequest
             'instagram' => ['nullable', 'url'],
             'telegram' => ['nullable', 'url'],
             'linkedin' => ['nullable', 'url'],
-            'whatsapp' => ['nullable', 'url']
+            'whatsapp' => ['nullable', 'url'],
+            'fa_owner_full_name' => ['required', 'max:100'],
+            'fa_owner_bio' => ['required'],
+            'fa_hero_title' => ['required', 'max:100'],
+            'en_owner_full_name' => ['required', 'max:100'],
+            'en_owner_bio' => ['required'],
+            'en_hero_title' => ['required', 'max:100'],
+            'owner_avatar' => ['max:1024']
         ];
     }
 
