bbc1df842bdc08f69e4bbd64c6e3d507d4335c88
AmirHosseinBabaei <ab8622822@gmail.com>
2026-08-17 11:06:37 +0330

    Add DashboardController Test

--- Modules/Auth/tests/Feature/DashboardTest.php
+++ Modules/Auth/tests/Feature/DashboardTest.php
@@ -1,16 +1,42 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Modules\Auth\Tests\Unit;
 
+use App\Models\User;
+use Modules\ContactUs\app\Models\ContactMessage;
+use Modules\Project\Models\Project;
+use Modules\Services\Models\Service;
+use Modules\Slider\Models\Slider;
 use Tests\TestCase;
 
 class DashboardTest extends TestCase
 {
-    /**
-     * A basic test example.
-     */
-    public function test_that_true_is_true(): void
+
+    public function test_admin_can_see_dashboard(): void
     {
-        $this->assertTrue(true);
+        $user = User::factory()->create();
+
+        //Login User.
+        $response = $this->actingAs($user)
+            ->get((app()->getLocale() . ".dashboard"));
+
+        //Check response view data.
+        $response->assertViewHas('counters', [
+            'users' => User::count(),
+            'projects' => Project::count(),
+            'contact_messages_count' => ContactMessage::count(),
+            'services' => Service::count(),
+        ]);
+
+        $response->assertViewHas('latest_projects', Project::latest()
+            ->take(3)->get());
+        $response->assertViewHas('latest_services', Service::latest()
+            ->take(3)->get());
+        $response->assertViewHas('latest_sliders', Slider::latest()
+            ->take(3)->get());
+
+        $response->assertOk();
     }
 }
