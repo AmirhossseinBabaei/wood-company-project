6aa89c950dc219792d9507374dd754425fb812f7
AmirHosseinBabaei <ab8622822@gmail.com>
2026-08-17 11:02:43 +0330

    Add PHPDOC and other comments to LoginController

--- Modules/Auth/app/Http/Controllers/LoginController.php
+++ Modules/Auth/app/Http/Controllers/LoginController.php
@@ -5,31 +5,39 @@ declare(strict_types=1);
 namespace Modules\Auth\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\View\View;
 use Modules\Auth\Http\Requests\LoginRequest;
 use Modules\Auth\Http\Services\LoginService;
 
 class LoginController extends Controller
 {
     /**
-     * show login form
+     * @return View
      */
-    public function showForm()
+    public function showForm(): View
     {
         return view('auth::login');
     }
 
     /**
-     * login logic
+     * @param LoginRequest $request
+     * @param LoginService $loginService
+     * @return RedirectResponse
      */
-    public function login(LoginRequest $request, LoginService $loginService)
+    public function login(LoginRequest $request, LoginService $loginService): RedirectResponse
     {
         $data = $request->validated();
+
         $loginStatus = $loginService->login($data);
 
-        if(false == $loginStatus) {
-            return view('auth::login')->with('error', __('Auth::messages.login_failed'));
+        //Check login failed
+        if (false == $loginStatus) {
+
+            return to_route('auth.login')->with('error', __('Auth::words.login_failed'));
         }
 
+        //Redirecting with successfully login to dashboard with current lang.
         return to_route((app()->getLocale() . '.dashboard'));
     }
 }
