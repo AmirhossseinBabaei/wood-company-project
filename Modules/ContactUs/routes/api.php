4b6e0a4486f7bcb7a07ba7be69132d20da9c1e39
AmirHosseinBabaei <ab8622822@gmail.com>
2026-08-18 12:15:09 +0330

    Cleaning routes

--- Modules/ContactUs/routes/api.php
+++ Modules/ContactUs/routes/api.php
@@ -1,8 +1,3 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use Modules\ContactUs\Http\Controllers\ContactUsController;
-
-Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
-    Route::apiResource('contactuses', ContactUsController::class)->names('contactus');
-});
+//api routes
--- Modules/Slider/routes/api.php
+++ Modules/Slider/routes/api.php
@@ -1,8 +1,3 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use Modules\Slider\Http\Controllers\SliderController;
-
-Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
-    Route::apiResource('sliders', SliderController::class)->names('slider');
-});
+//api routes
